cd6a65a189ec210103bcdd6252e0d7ebf29618da
ArcanumLibella <fiona.roux@hetic.net>
2019-03-13 12:41:55 +0100

    test preview opengraph

--- src/js/script.js
+++ src/js/script.js
@@ -3,7 +3,7 @@
 */
 
 document.querySelector(".menu__button").addEventListener("click", function( event ) {
-  document.querySelector(".menu__nav").classList.toggle('menu--show');
+  document.querySelector(".menu__nav").classList.toggle('menu__nav--show');
 
  document.querySelector(".menu__button").classList.toggle('menu__button--show');
 });

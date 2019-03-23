1cac8913499398349ea259483f26e6ef96a19fc5
ArcanumLibella <fiona.roux@hetic.net>
2019-03-23 18:02:49 +0100

    Icons social hover and SVG sprite

--- src/js/script.js
+++ src/js/script.js
@@ -2,9 +2,17 @@
   MENU BURGER
 */
 
-document.querySelector(".menu__button").addEventListener("click", function( event ) {
-  document.querySelector(".menu__nav").classList.toggle('menu__nav--show');
-
- document.querySelector(".menu__button").classList.toggle('menu__button--show');
+let menuButton = document.getElementById("menu__button");menuButton.addEventListener("click", function() {
+  let menuNav = document.querySelector(".menu__nav").classList.toggle('is-open');
+  menuButton.classList.toggle('is-clicked');
 });
 
+
+
+
+
+// document.getElementById("menu__button").addEventListener("click", function( event ) {
+//   document.querySelector(".menu__nav").classList.toggle('menu__nav--show');
+
+//  document.getElementById("menu__button").classList.toggle('menu__button--show');
+// });

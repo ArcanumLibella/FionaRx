f406a03d2382a29dad6f873c5b826f14ef7b858f
ArcanumLibella <fiona.roux@hetic.net>
2019-03-28 12:08:33 +0100

    add skills and about

--- src/js/script.js
+++ src/js/script.js
@@ -2,7 +2,8 @@
   MENU BURGER
 */
 
-let menuButton = document.getElementById("menu__button");menuButton.addEventListener("click", function() {
+let menuButton = document.getElementById("menu__button");
+  menuButton.addEventListener("click", function() {
   let menuNav = document.querySelector(".menu__nav").classList.toggle('is-open');
   menuButton.classList.toggle('is-clicked');
 });

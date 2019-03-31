1000d55a62e93f75da936c8564d497293dde9c95
ArcanumLibella <fiona.roux@hetic.net>
2019-03-31 16:44:49 +0200

    add background image on desktop version

--- src/js/script.js
+++ src/js/script.js
@@ -1,24 +1,36 @@
-/* 
-* MENU BURGER
-*/
+/* DECLARATION DE VARIABLES */
 
+// Récupération de toutes les ancres :
+let links = document.querySelectorAll("a[href^='#']");
 let menuButton = document.getElementById("menu__button");
+
+
+
+/* 
+*  BURGER MENU
+*/
+// Ouverture/Fermeture via le bouton Menu
   menuButton.addEventListener("click", function() {
   let menuNav = document.querySelector(".menu__nav").classList.toggle('is-open');
   menuButton.classList.toggle('is-clicked');
 });
 
+// Fermeture après click sur un lien
+  for (let index = 0; index < links.length; index++) {
+    let link = links[index];
+    link.addEventListener("click", function() {
+      document.querySelector(".menu__nav").classList.toggle('is-open');
+    })
+  }
+
 /* 
 * SMOOTH SCROLLING NAVIGATION
 */
 
-// Récupération de toutes les ancres :
-var links = document.querySelectorAll("a[href^='#']");
 // Durée du scroll :
 var duration = 1200;
 
 for (let i = 0; i < links.length; i++)
-
   links[i].addEventListener("click", function(event) {
   var start = new Date;
   var from  = window.pageYOffset; // position Y départ dans la page
@@ -29,7 +41,6 @@ for (let i = 0; i < links.length; i++)
     window.scrollTo(0, (to - from) * Math.pow(progress, 2) + from);  
     (progress < 1) && window.requestAnimationFrame(scroll);
   }
-
   scroll();
-
 }, false);
+

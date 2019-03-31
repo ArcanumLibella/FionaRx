1ca5f045038dcdf03133bccedf2f4b4c8423da43
ArcanumLibella <fiona.roux@hetic.net>
2019-03-31 15:28:19 +0200

    review hero on mobile

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,5 @@
 /* 
-  MENU BURGER
+* MENU BURGER
 */
 
 let menuButton = document.getElementById("menu__button");
@@ -8,12 +8,28 @@ let menuButton = document.getElementById("menu__button");
   menuButton.classList.toggle('is-clicked');
 });
 
+/* 
+* SMOOTH SCROLLING NAVIGATION
+*/
 
+// Récupération de toutes les ancres :
+var links = document.querySelectorAll("a[href^='#']");
+// Durée du scroll :
+var duration = 1200;
 
+for (let i = 0; i < links.length; i++)
 
+  links[i].addEventListener("click", function(event) {
+  var start = new Date;
+  var from  = window.pageYOffset; // position Y départ dans la page
+  var to = document.getElementById(this.hash.substring(1)).offsetTop;
+  event.preventDefault();
+  var scroll = function() {
+    var progress = Math.min(1, (new Date - start) / duration);
+    window.scrollTo(0, (to - from) * Math.pow(progress, 2) + from);  
+    (progress < 1) && window.requestAnimationFrame(scroll);
+  }
 
-// document.getElementById("menu__button").addEventListener("click", function( event ) {
-//   document.querySelector(".menu__nav").classList.toggle('menu__nav--show');
+  scroll();
 
-//  document.getElementById("menu__button").classList.toggle('menu__button--show');
-// });
+}, false);
